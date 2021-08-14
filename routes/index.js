103805a241846fb568b722d6b3c50b95d18e061f
Jonathan Park <johanpk10@gmail.com>
2021-08-14 13:28:21 -0700

    Add routing and new message posting functionalities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var messages = [
+  {
+    text: "Hello World!",
+    user: "Java",
+    added: new Date()
+  },
+  {
+    text: "Who am I?",
+    user: "Merlin",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function(req, res, next) {
+  var messageData = req.body;
+
+  messages.push(
+    {
+      text: messageData.text,
+      user: messageData.user,
+      added: new Date()
+    }
+  );
+
+  res.redirect('/');
 });
 
 module.exports = router;
